05179a4d51c1f2e1fd640f97c82478d56e4146b7
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-07-13 17:02:19 -0700

    complete live sample button listeners, will redirect to live gifs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,41 @@
-var repoButton = document.getElementById('linksToWork')
+var gifBtn1 = document.getElementById('gif1')
+var gifBtn2 = document.getElementById('gif2')
+var gifBtn3 = document.getElementById('gif3')
+var displayGifs = $('.showGif')
 
 //add event listeners
-repoButton.addEventListener('click', function (event) {
-    console.log(event)
+gifBtn1.addEventListener('click', function (event) {
+    console.log(event);
+    displayGifs.empty();
+    
+    createImg = $('<img>');
+    createImg.attr('src', "./assets/images/Sample repo images/gifs/TWH_demo.gif");
+    createImg.attr('alt', "Three word history demo");
+    createImg.addClass('exampleGif');
+
+    displayGifs.append(createImg);
+})
+
+gifBtn2.addEventListener('click', function (event) {
+    console.log(event);
+    displayGifs.empty();
+    
+    createImg = $('<img>');
+    createImg.attr('src', "./assets/images/Sample repo images/gifs/weather_dashboard_example.gif");
+    createImg.attr('alt', "weather dashboard gif");
+    createImg.addClass('exampleGif');
+
+    displayGifs.append(createImg);
+})
+
+gifBtn3.addEventListener('click', function (event) {
+    console.log(event);
+    displayGifs.empty();
+    
+    createImg = $('<img>');
+    createImg.attr('src', "./assets/images/Sample repo images/gifs/codequizchallengeDemo.gif");
+    createImg.attr('alt', "codequizchallenge gif");
+    createImg.addClass('exampleGif');
+
+    displayGifs.append(createImg);
 })
